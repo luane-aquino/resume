201615afa734bb1eeb175a6ff814af91bd869228
Luane Aquino <luanecaroline91@gmail.com>
2020-04-07 11:04:06 -0300

    Updates

---
+++ precache-manifest.f6cd80f4993efe1a836f93d3f93323a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae14218d4e42eb3030bc71dcae72b9c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "3e744f3716caa928060d",
+    "url": "/resume/static/css/main.74e848ea.chunk.css"
+  },
+  {
+    "revision": "173a99a918f7a3a159b7",
+    "url": "/resume/static/js/2.0b902bcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.0b902bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e744f3716caa928060d",
+    "url": "/resume/static/js/main.603c10cc.chunk.js"
+  },
+  {
+    "revision": "0ab38462de942daee485",
+    "url": "/resume/static/js/runtime-main.aada0527.js"
+  },
+  {
+    "revision": "e7bf5bf0056f315358c9e55f97095b8d",
+    "url": "/resume/static/media/profile_picture.e7bf5bf0.png"
+  }
+]);
